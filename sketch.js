a2c6609c1668ef2e83374bd862b5a1cd741e2f22
way0331 <75355930+way0331@users.noreply.github.com>
2020-12-02 15:19:09 +0800

    1202
    A!

--- sketch.js
+++ sketch.js
@@ -16,7 +16,7 @@ let classifier;
 let label = "listening";
 
 // Teachable Machine model URL:
-let soundModelURL = 'https://teachablemachine.withgoogle.com/models/h3p9R41J/model.json';
+let soundModelURL = 'https://way0331.github.io/soundcl/index.html';
 
 
 function preload() {
